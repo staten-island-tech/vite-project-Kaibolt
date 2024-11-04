d4d11ab7b1d2a3e756870277a00454603af48cd1
Kaibolt <kyle.salowitz08@gmail.com>
2024-11-04 12:27:57 -0500

    Mode & Button update
    Modes have successfully been added. Buttons have been added to swap between arrays.
    Next Steps: ADD THE GODDAMN ARRAYS! And the buttons to swap between them. 🤪

--- vite-project/JS/main.js
+++ vite-project/JS/main.js
@@ -1,19 +1,13 @@
 import "../CSS/style.css";
-document.querySelector("#darkMode").addEventListener("click", function () {
-  const changer = document.querySelector("#darkMode");
-  if (document.body.classList.contains("dark")) {
-    document.body.classList.add("light");
-    document.body.classList.remove("dark");
-    changer.innerHTML = "Light Mode";
-  }
-  if (document.body.classList.contains("light")) {
-    document.body.classList.add("cool");
-    document.body.classList.remove("light");
-    changer.innerHTML = "Cool Mode";
-  }
-  if (document.body.classList.contains("cool")) {
-    document.body.classList.add("warm");
-    document.body.classList.remove("cool");
-    changer.innerHTML = "Warm Mode";
-  }
+document.querySelector(".darkMode").addEventListener("click", function () {
+  document.body.className = "light";
+});
+document.querySelector(".lightMode").addEventListener("click", function () {
+  document.body.className = "dark";
+});
+document.querySelector(".coolMode").addEventListener("click", function () {
+  document.body.className = "cool";
+});
+document.querySelector(".warmMode").addEventListener("click", function () {
+  document.body.className = "warm";
 });
