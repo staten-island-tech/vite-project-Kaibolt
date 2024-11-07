c01b10ddd9a7290f72e3e911f47f53554c8c7818
Kaibolt <144480358+Kaibolt@users.noreply.github.com>
2024-11-06 21:28:07 -0500

    Test in school?

--- vite-project/JS/main.js
+++ vite-project/JS/main.js
@@ -14,10 +14,15 @@ document.querySelector(".warmMode").addEventListener("click", function () {
   document.body.className = "warm";
 });
 function cardInserter() {
-  Animals.forEach((Object) => {
-    const cardHTML = `<div class="card"> <h1 class="card-header">Animal: ${Object.name}</h1> <h2 class="card-info">Price: ${Object.price}</h2> <h3 class="card-desc">Category: ${Object.category}</h3>"</div>`;
+  Animals.forEach((animal) => {
+    const cardHTML = `
+      <div class="card">
+        <h1 class="card-header">Animal: ${animal.name}</h1>
+        <h2 class="card-info">Price: ${animal.price}</h2>
+        <h3 class="card-desc">Category: ${animal.category}</h3>
+      </div>`;
+    DOMSelectors.container.insertAdjacentHTML("beforeend", cardHTML);
   });
-  DOMSelectors.container.insertAdjacentHTML("beforeend", cardHTML);
 }
 DOMSelectors.button.addEventListener("click", function () {
   cardInserter();
