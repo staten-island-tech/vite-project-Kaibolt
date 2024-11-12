632edd6a5239667d897b41320cebaa3290ae9481
Kaibolt <144480358+Kaibolt@users.noreply.github.com>
2024-11-11 22:48:52 -0500

    FINALLY WORKING
    COMPUTER FINALLY WORKS!!!!

--- vite-project/JS/main.js
+++ vite-project/JS/main.js
@@ -17,11 +17,31 @@ document.querySelector(".warmMode").addEventListener("click", function () {
 document.querySelector("#reset").addEventListener("click", function () {
   console.log("works");
 });
-animals.forEach((animal) =>
-  document
-    .querySelector(".cardContainer")
-    .insertAdjacentHTML(
-      "beforeend",
-      `<div class="card"><h1 class="card-header">Animal: ${animal.name}</h1><br><img class="card-img" src="${animal.img_url}" alt="${animal.alt_description}"><h2 class="card-desc">Price: ${animal.price}</h2><h3 class="card-desc">Category: ${animal.category}</h3></div>`
-    )
-);
+function defaultAnimal(animals) {
+  animals.forEach((animal) =>
+    document
+      .querySelector(".cardContainer")
+      .insertAdjacentHTML(
+        "beforeend",
+        `<div class="card"><h1 class="card-header">Animal: ${animal.name}</h1><br><img class="card-img" src="${animal.img_url}" alt="${animal.alt_description}"><h2 class="card-desc">Price: ${animal.price}</h2><h3 class="card-desc">Category: ${animal.category}</h3></div>`
+      )
+  );
+}
+defaultAnimal(animals);
+document.querySelector("#reset").addEventListener("click", function () {
+  document.querySelector(".cardContainer").innerHTML = "";
+  defaultAnimal(animals);
+});
+
+document.querySelector("#nocSale").addEventListener("click", function () {
+  const nocOnly = animals.filter((animal) => animal.category === "Nocturnal");
+  document.querySelector(".cardContainer").innerHTML = "";
+  defaultAnimal(nocOnly);
+});
+
+//document.querySelector("#mamSale").addEventListener("click", function () {
+//  const allcate = animals.map((animal) => animal.category);
+//  const mamOnly = allcate.includes("Mammal");
+//  document.querySelector(".cardContainer").innerHTML = "";
+//  defaultAnimal(mamOnly);
+//});
